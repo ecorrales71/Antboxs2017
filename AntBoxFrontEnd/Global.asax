8b1bd2a82ebed265cabcd7f754bb78ac1d7f9773
1046986683 <1046986683@EMC>
2016-07-14 21:25:26 -0500

    Add project files.

---
+++ AntBoxFrontEnd/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AntBoxFrontEnd.MvcApplication" Language="C#" %>
---
+++ AntBoxFrontEnd/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AntBoxFrontEnd.Startup))]
+namespace AntBoxFrontEnd
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
