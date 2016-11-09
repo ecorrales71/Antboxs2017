76fb4390abd3b8bfda64cd03f81d051b8cd056ee
Elizardo Velasquez <elizardo.ve@gmail.com>
2016-11-09 07:26:44 -0430

    Agregar - Editar direccion de facturacion

--- AntBoxFrontEnd/Services/BillingAddress/BillingAddressRequestOptions.cs
+++ AntBoxFrontEnd/Services/BillingAddress/BillingAddressRequestOptions.cs
@@ -17,7 +17,34 @@ namespace AntBoxFrontEnd.Services.BillingAddress
         [JsonProperty("rfc_id")]
         public string Rfc_id { get; set; }
 
-        [JsonProperty("references")]
-        public string References { get; set; }
+        [JsonProperty("zipcode")]
+        public string Zipcode { get; set; }
+
+        [JsonProperty("street")]
+        public string Street { get; set; }
+
+        [JsonProperty("external_number")]
+        public string External_number { get; set; }
+
+        [JsonProperty("internal_number")]
+        public string Internal_number { get; set; }
+
+        [JsonProperty("neighbourhood")]
+        public string Neighborhood { get; set; }
+
+        [JsonProperty("state")]
+        public string State { get; set; }
+
+        [JsonProperty("delegation")]
+        public string Delegation { get; set; }
+
+        [JsonProperty("city")]
+        public string City { get; set; }
+
+        [JsonProperty("country")]
+        public string Country { get; set; }
+
+        [JsonProperty("billing_email")]
+        public string Billing_email { get; set; }
     }
 }
--- AntBoxFrontEnd/Services/BillingAddress/BillingAddressUpdateOptions.cs
+++ AntBoxFrontEnd/Services/BillingAddress/BillingAddressUpdateOptions.cs
@@ -16,5 +16,35 @@ namespace AntBoxFrontEnd.Services.BillingAddress
 
         [JsonProperty("references")]
         public string References { get; set; }
+
+        [JsonProperty("zipcode")]
+        public string Zipcode { get; set; }
+
+        [JsonProperty("street")]
+        public string Street { get; set; }
+
+        [JsonProperty("external_number")]
+        public string External_number { get; set; }
+
+        [JsonProperty("internal_number")]
+        public string Internal_number { get; set; }
+
+        [JsonProperty("neighbourhood")]
+        public string Neighborhood { get; set; }
+
+        [JsonProperty("state")]
+        public string State { get; set; }
+
+        [JsonProperty("delegation")]
+        public string Delegation { get; set; }
+
+        [JsonProperty("city")]
+        public string City { get; set; }
+
+        [JsonProperty("country")]
+        public string Country { get; set; }
+
+        [JsonProperty("billing_email")]
+        public string Billing_email { get; set; }
     }
 }
